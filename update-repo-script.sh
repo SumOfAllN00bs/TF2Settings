04579c842a429d87f54f77f95ffa50d3bbf7f671
kharvey-nz <kyleajk@hotmail.com>
2019-01-13 04:57:36 +1300

    Overhaul of update script. auto remove old files
    Added auto zipping of custom folder.
    Added auto removing of custom.zip and cfg/.
    Tidied up the update script.

--- update-repo-script.sh
+++ update-repo-script.sh
@@ -1,3 +1,11 @@
-cp -r ~/.steam/steam/steamapps/common/Team\ Fortress\ 2/tf/cfg/ .
+TFPATH=~/.steam/steam/steamapps/common/Team\ Fortress\ 2/tf
+CURPATH=`pwd`
+rm custom.zip
+rm -r cfg/
+cp -r "$TFPATH/cfg/" .
+cd "$TFPATH"
+zip -r custom.zip custom/ -x custom/disabled/
+mv custom.zip "$CURPATH"
+cd "$CURPATH"
 git add .
 git commit && git push -u origin master
